98f2d0beb70388dfdaec723d6f8d0925af319df2
Laluarkan <lalunaufalarkan21@gmail.com>
2026-04-07 09:20:00 +0800

    Update build.sh untuk auto-migrate dan auto-createsuperuser

--- build.sh
+++ build.sh
@@ -3,5 +3,10 @@
 set -o errexit
 
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Membuat superuser secara otomatis dari Environment Variables (mode non-interaktif)
+# || true digunakan agar build tidak gagal jika user admin sudah pernah dibuat sebelumnya
+python manage.py createsuperuser --noinput || true
